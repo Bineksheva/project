94b1231bcbda4eb7ad008621cc1e25b5dfc52805
Bineksheva <shevochk@mail.ua>
2014-06-20 18:03:34 +0300

    " "

--- sites/all/themes/thoughtbuzz/templates/block.tpl.php
+++ sites/all/themes/thoughtbuzz/templates/block.tpl.php
@@ -1,9 +1,9 @@
 <?php $tag = $block->subject ? 'section' : 'div'; ?>
 <<?php print $tag; ?><?php print $attributes; ?>>
-  <div class="block-inner clearfix">
+  <div class="block-inner clearfix thoughtBuzz_block_title">
     <?php print render($title_prefix); ?>
     <?php if ($block->subject): ?>
-      <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+      <h2<?php print $title_attributes;?>><?php print $block->subject; ?></h2>
     <?php endif; ?>
     <?php print render($title_suffix); ?>
     
