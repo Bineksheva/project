ff9a0952de30c14ac5de0090a51c0b0be86f08f1
Bineksheva <shevochk@mail.ua>
2014-06-20 18:03:21 +0300

    " "

---
+++ sites/all/themes/thoughtbuzz/templates/region--footer_first.tpl.php
@@ -0,0 +1,6 @@
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <?php print $content; ?>
+    <div class="thoughtBuzz_copyright">Copyright 2014 ThoughtBuzz</div>
+  </div>
+</div>
